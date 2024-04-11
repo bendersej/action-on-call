67063ec91abc3670211ec8ea398deeee45fdf2a8
Benjamin André-Micolon <benjamin@robocorp.com>
2024-04-11 15:42:13 +0300

    fix: secret value

--- action.py
+++ action.py
@@ -11,9 +11,9 @@ def who_is_on_call_today(alertops_group_id: Secret, alertops_api_key: Secret) ->
     """
     Prints the current engineer on call
     """
-    api_url = f"https://app.alertops.com/api/v2/schedules/oncallnow?group_id={alertops_group_id}"
+    api_url = f"https://app.alertops.com/api/v2/schedules/oncallnow?group_id={alertops_group_id.value}"
 
-    headers = {"api-key": alertops_api_key}
+    headers = {"api-key": alertops_api_key.value}
 
     response = requests.get(api_url, headers=headers)
     engineer_on_call = response.json()[0]["members"][0]
